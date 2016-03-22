17df4fba5bad001baa619a29b9f54fbe92d1fcf0
Solbon Daribazaron <s.daribazaron@visiona.pl>
2016-03-22 10:46:32 +0100

    Concurrent test files

---
+++ Tests/src/tests/dateJava8/ZooPeriod.java
@@ -0,0 +1,22 @@
+package tests.dateJava8;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ * @author Solbon Daribazaron <s.daribazaron@visiona.pl>
+ */
+public class ZooPeriod {
+    public static void main(String[] args) {
+        LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+        LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+        performAnimalEnrichment(start, end);
+    }
+    private static void performAnimalEnrichment(LocalDate start, LocalDate end) {
+        LocalDate upTo = start;
+        while (upTo.isBefore(end)) { // check if still before end
+            System.out.println("give new toy: " + upTo);
+            upTo = upTo.plusMonths(1); // add a month
+        }
+    }
+}
